880c732507e7c45ac4d2668d731b8280adae8ee3
MartinAlejandroBaer <134695996+MartinAlejandroBaer@users.noreply.github.com>
2024-05-13 12:27:51 +0200

    Update opcuaExample.py

--- opcuaExample.py
+++ opcuaExample.py
@@ -9,7 +9,7 @@ async def main():
     server = Server() # setup our server
     await server.init()
     server.set_endpoint('opc.tcp://XXX.XXX.XXX.XXX:4840/') # Setting the IP and port
-    await server.import_xml("AASmodelV2.xml") # Importing the OPCUA ML model generated with the AASX tool.
+    await server.import_xml("AASmodelV2.xml") # Importing the OPCUA ML model generated with the AASX tool. (In this case AASmodelV2.xml will the imported)
     # Creating the variables based on the nodes of the OPCUA ML model. 
     # Use the UaModeler or opcua-modeler to verify the number of the nodes.
     Temperature=server.get_node("ns=3;i=119")
