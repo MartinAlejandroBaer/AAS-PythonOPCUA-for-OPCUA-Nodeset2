c5d97f7344bcecf60b8c46b348de0ea54410351f
MartinAlejandroBaer <134695996+MartinAlejandroBaer@users.noreply.github.com>
2024-08-19 10:17:00 +0200

    Create server.py

---
+++ examples/basic_example_method_operation/server.py
@@ -0,0 +1,40 @@
+import logging
+import asyncio
+import sys
+sys.path.insert(0, "..")
+ # importing the Library opcua from FreeOPC-UA.
+from asyncua import uamethod, Server
+
+@uamethod
+def methodScript(parent, x):
+    return 5*x 
+
+import datetime # Library to generate a date
+async def main():
+    _logger = logging.getLogger('asyncua')
+    # setup our server
+    server = Server()
+    await server.init()
+    server.set_endpoint('opc.tcp://192.168.200.181:4840/') # Setting the IP and port
+    await server.import_xml("AASmodel.xml") # Importing the OPCUA ML model generated on the AASX tool.
+    # Creating the variables based on the nodes defined in the OPCUA ML model.
+    testProperty1=server.get_node("ns=3;i=119")
+    testProperty2=server.get_node("ns=3;i=126")
+    # We have now to set up the varibales to editable if we want to overwrite its values
+    testProperty1.set_writable()
+    testProperty1.set_writable()
+    # We can associate a method in OPC UA with an Operation of the AAS.
+    # The following lines will link such operation with the defined method "methodScript"
+    methodXML = server.get_node("ns=3;i=148")
+    server.link_method(methodXML,methodScript)   
+    async with server:
+        while True: # Infinite loop to overwrite the variables Temperature and Volume
+            await asyncio.sleep(3)
+            new_testProperty1 = await testProperty1.get_value() + 0.1
+            new_testProperty2 = await testProperty2.get_value() + 1
+            await testProperty1.write_value(new_testProperty1)
+            await testProperty2.write_value(new_testProperty2)
+            print("New testProperty1: " + str(new_testProperty1) + ", New testProperty2: " + str(new_testProperty2))          
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    asyncio.run(main(), debug=True)
